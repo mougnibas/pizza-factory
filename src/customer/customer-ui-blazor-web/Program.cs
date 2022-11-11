3c5497e4d5c9fd2340d7610327fbc8a9925b571e
Yoann MOUGNIBAS <yoann@mougnibas.fr>
2022-11-11 20:06:03 +0100

    Add embeded service

--- src/customer/customer-ui-blazor-web/Program.cs
+++ src/customer/customer-ui-blazor-web/Program.cs
@@ -1,17 +1,16 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Mougnibas.PizzaFactory.Customer.Business;
+using Mougnibas.PizzaFactory.Customer.Contract;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 
-var app = builder.Build();
+// Add integrated service (not the microservice version)
+builder.Services.AddSingleton<IService, ServiceImpl>();
 
-if (!app.Environment.IsDevelopment())
-{
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
+var app = builder.Build();
 
 app.UseHttpsRedirection();
 
